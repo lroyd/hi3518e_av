557b16994fcde00478e4d961d2a39bf63081b327
zhanghaitao <lroyd-zhang@systechn.com>
2018-11-12 18:44:32 -0800

    V2.1 add main.c for test

--- hi3518e.c
+++ hi3518e.c
@@ -6,6 +6,7 @@
  ************************************************************************/
 #include "hi3518e.h"
 #include "hi3518e_audio.h"
+#include "hi3518e_video.h"
 #include "hi3518e_device.h"
 #include "hi3518e_debug.h"
 
@@ -540,58 +541,5 @@ int HI3515E_DeviceInit(void)
 	return iRet;
 }
 
-
-/******************************************************************************
-*测试代码
-******************************************************************************/
-FILE *file = NULL;
-
-int audio_data(char *p_data, int len)
-{
-	char audio_pt[320] = {0};
-
-	memcpy(audio_pt, p_data, 320);
-	HI3515E_AudioPlayer(audio_pt, NULL);
-	
-	
-	return 0;
-}
-
-
-
-int video_data(int naltype, char *p_data, int len)
-{
-
-	printf("nal type %d, len %d\r\n", naltype, len);
-	//fwrite(p_data, len, 1, file);
-	//fflush(file);	
-	
-	return 0;
-}
-
-
-
-int main()
-{
-	int iRet = 0;
-	//保存视频文件
-	file = fopen("test.h264", "wb");
-	
-	iRet = HI3515E_DeviceInit();
-	printf("device init %d\r\n", iRet);
-	HI3518E_VideoEnable();
-	//iRet = HI3518E_AudioEnable(0);	//不bind，可以使用bind模式来测试声音
-	//printf("audio init %d.", iRet);
-	//HI3515E_AudioSStart(audio_data);
-	
-	
-	HI3515E_VideoSStart(video_data);
-	
-	while(1)
-	{
-		sleep(1);
-	}
-	return 0;
-}
 
 
